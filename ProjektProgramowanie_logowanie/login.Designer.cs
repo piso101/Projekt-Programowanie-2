d8dadf7bc06fa3346d021efa3c0211d2cff254eb
Olobonkolo <olaf.gruszkiewicz@gmail.com>
2023-03-21 23:37:42 +0100

    Logowanie done (brak poprawek graficznych)
    trzeba doszlifować

--- ProjektProgramowanie_logowanie/login.Designer.cs
+++ ProjektProgramowanie_logowanie/login.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             this.registerButton = new System.Windows.Forms.Button();
+            this.zalogujButton = new System.Windows.Forms.Button();
+            this.loginuserBox = new System.Windows.Forms.TextBox();
+            this.passworduserBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // registerButton
@@ -41,21 +44,53 @@
             this.registerButton.UseVisualStyleBackColor = true;
             this.registerButton.Click += new System.EventHandler(this.registerButton_Click);
             // 
+            // zalogujButton
+            // 
+            this.zalogujButton.Location = new System.Drawing.Point(287, 325);
+            this.zalogujButton.Name = "zalogujButton";
+            this.zalogujButton.Size = new System.Drawing.Size(93, 23);
+            this.zalogujButton.TabIndex = 1;
+            this.zalogujButton.Text = "button1";
+            this.zalogujButton.UseVisualStyleBackColor = true;
+            this.zalogujButton.Click += new System.EventHandler(this.zalogujButton_Click);
+            // 
+            // loginuserBox
+            // 
+            this.loginuserBox.Location = new System.Drawing.Point(321, 190);
+            this.loginuserBox.Name = "loginuserBox";
+            this.loginuserBox.Size = new System.Drawing.Size(169, 22);
+            this.loginuserBox.TabIndex = 2;
+            // 
+            // passworduserBox
+            // 
+            this.passworduserBox.Location = new System.Drawing.Point(321, 245);
+            this.passworduserBox.Name = "passworduserBox";
+            this.passworduserBox.Size = new System.Drawing.Size(169, 22);
+            this.passworduserBox.TabIndex = 3;
+            this.passworduserBox.TextChanged += new System.EventHandler(this.passworduserBox_TextChanged);
+            // 
             // login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::projektProgramowanie_logowanie.Properties.Resources.Logowanie;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.Controls.Add(this.passworduserBox);
+            this.Controls.Add(this.loginuserBox);
+            this.Controls.Add(this.zalogujButton);
             this.Controls.Add(this.registerButton);
             this.Name = "login";
             this.Size = new System.Drawing.Size(797, 452);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button registerButton;
+        private System.Windows.Forms.Button zalogujButton;
+        private System.Windows.Forms.TextBox loginuserBox;
+        private System.Windows.Forms.TextBox passworduserBox;
     }
 }
--- ProjektProgramowanie_logowanie/login.cs
+++ ProjektProgramowanie_logowanie/login.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 namespace projektProgramowanie_logowanie
 {
@@ -17,12 +20,70 @@ namespace projektProgramowanie_logowanie
             InitializeComponent();
         }
 
+        SqlConnection sqlcon = new SqlConnection(@"Server=tcp:onlinegradebook.database.windows.net,1433;Initial Catalog=StoMaToLogiczne;Persist Security Info=False;User ID=theedziu;Password=Kacper123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        SqlCommand cmd;
+        SqlDataReader dr;
+
         private void registerButton_Click(object sender, EventArgs e)
         {
             register register = new register(); //nadajemy instancje
             this.Parent.Controls.Add(register); // user control(login) jest rodzicem wzglédem registera
             register.BringToFront(); //po kliknieciu wybierze go  na front i odda miejsce w hierarchi dla registera
             this.Parent.Controls.Remove(this);
+        }
+
+        private void passworduserBox_TextChanged(object sender, EventArgs e)
+        {
+            passworduserBox.UseSystemPasswordChar = true;
+        }
+
+        private void zalogujButton_Click(object sender, EventArgs e)
+        {
+            string passwordt = passworduserBox.Text;
+            string logint = loginuserBox.Text;
+
+            try
+            {
+                
+                cmd = new SqlCommand ("SELECT * FROM Users WHERE  login = @LOGIN AND password = @PASSWORD", sqlcon);
+                cmd.Parameters.AddWithValue("@LOGIN" , logint );
+                cmd.Parameters.AddWithValue("@PASSWORD", passwordt);
+
+                sqlcon.Open();
+
+                dr = cmd.ExecuteReader();
+
+
+                if (dr.HasRows)
+                {
+
+
+                    MessageBox.Show("Witaj!");
+
+                    projektProgramowanie_logowanie.Form1 f1 = new projektProgramowanie_logowanie.Form1();
+                    ProjektProgramowanie_MainPage.Form1 f2 = new ProjektProgramowanie_MainPage.Form1();
+                    f1.Hide();
+                    f2.Show();
+
+
+
+                }
+                else
+                {
+                    MessageBox.Show("nieprawidłowy login lub hasło", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    passworduserBox.Clear();
+
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Brak połączenia.");
+            }
+            finally
+            { 
+                sqlcon.Close(); 
+            }
+            
         }
     }
 }
