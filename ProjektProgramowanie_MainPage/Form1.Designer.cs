93db34de04b4e2a7a3ca4723c7d77428334d98fc
KatarzynaKowalczyk97 <121797910+KatarzynaKowalczyk97@users.noreply.github.com>
2023-05-09 07:42:46 +0200

    09.05 7:42

--- ProjektProgramowanie_MainPage/Form1.Designer.cs
+++ ProjektProgramowanie_MainPage/Form1.Designer.cs
@@ -28,87 +28,96 @@
         /// </summary>
         private void InitializeComponent()
         {
-			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-			this.doctorDataGridView = new System.Windows.Forms.DataGridView();
-			this.Combobox_miasta = new System.Windows.Forms.ComboBox();
-			this.label1 = new System.Windows.Forms.Label();
-			this.Umowionespotkaniabtn = new System.Windows.Forms.Button();
-			this.umow_wizyte_btn = new System.Windows.Forms.Button();
-			this.umowione_wizyty1 = new ProjektProgramowanie_MainPage.Umowione_wizyty();
-			((System.ComponentModel.ISupportInitialize)(this.doctorDataGridView)).BeginInit();
-			this.SuspendLayout();
-			// 
-			// doctorDataGridView
-			// 
-			this.doctorDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-			this.doctorDataGridView.Location = new System.Drawing.Point(26, 75);
-			this.doctorDataGridView.Name = "doctorDataGridView";
-			this.doctorDataGridView.Size = new System.Drawing.Size(219, 162);
-			this.doctorDataGridView.TabIndex = 1;
-			// 
-			// Combobox_miasta
-			// 
-			this.Combobox_miasta.FormattingEnabled = true;
-			this.Combobox_miasta.Location = new System.Drawing.Point(39, 290);
-			this.Combobox_miasta.Name = "Combobox_miasta";
-			this.Combobox_miasta.Size = new System.Drawing.Size(205, 21);
-			this.Combobox_miasta.TabIndex = 2;
-			// 
-			// label1
-			// 
-			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(39, 25);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(35, 13);
-			this.label1.TabIndex = 3;
-			this.label1.Text = "label1";
-			// 
-			// Umowionespotkaniabtn
-			// 
-			this.Umowionespotkaniabtn.Location = new System.Drawing.Point(52, 360);
-			this.Umowionespotkaniabtn.Name = "Umowionespotkaniabtn";
-			this.Umowionespotkaniabtn.Size = new System.Drawing.Size(121, 21);
-			this.Umowionespotkaniabtn.TabIndex = 4;
-			this.Umowionespotkaniabtn.Text = "umowione spotkania";
-			this.Umowionespotkaniabtn.UseVisualStyleBackColor = true;
-			this.Umowionespotkaniabtn.Click += new System.EventHandler(this.Umowionespotkaniabtn_Click);
-			// 
-			// umow_wizyte_btn
-			// 
-			this.umow_wizyte_btn.Location = new System.Drawing.Point(52, 387);
-			this.umow_wizyte_btn.Name = "umow_wizyte_btn";
-			this.umow_wizyte_btn.Size = new System.Drawing.Size(121, 21);
-			this.umow_wizyte_btn.TabIndex = 5;
-			this.umow_wizyte_btn.Text = "umow wizyte";
-			this.umow_wizyte_btn.UseVisualStyleBackColor = true;
-			this.umow_wizyte_btn.Click += new System.EventHandler(this.umow_wizyte_btn_Click);
-			// 
-			// umowione_wizyty1
-			// 
-			this.umowione_wizyty1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("umowione_wizyty1.BackgroundImage")));
-			this.umowione_wizyty1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-			this.umowione_wizyty1.Location = new System.Drawing.Point(-3, -2);
-			this.umowione_wizyty1.Name = "umowione_wizyty1";
-			this.umowione_wizyty1.Size = new System.Drawing.Size(778, 444);
-			this.umowione_wizyty1.TabIndex = 0;
-			// 
-			// Form1
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(774, 444);
-			this.Controls.Add(this.umow_wizyte_btn);
-			this.Controls.Add(this.Umowionespotkaniabtn);
-			this.Controls.Add(this.label1);
-			this.Controls.Add(this.Combobox_miasta);
-			this.Controls.Add(this.doctorDataGridView);
-			this.Controls.Add(this.umowione_wizyty1);
-			this.Name = "Form1";
-			this.Text = "Form1";
-			this.Load += new System.EventHandler(this.Form1_Load);
-			((System.ComponentModel.ISupportInitialize)(this.doctorDataGridView)).EndInit();
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.doctorDataGridView = new System.Windows.Forms.DataGridView();
+            this.Combobox_miasta = new System.Windows.Forms.ComboBox();
+            this.Umowionespotkaniabtn = new System.Windows.Forms.Button();
+            this.umow_wizyte_btn = new System.Windows.Forms.Button();
+            this.Szukaj_btn = new System.Windows.Forms.Button();
+            this.umowione_wizyty1 = new ProjektProgramowanie_MainPage.Umowione_wizyty();
+            ((System.ComponentModel.ISupportInitialize)(this.doctorDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // doctorDataGridView
+            // 
+            this.doctorDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.doctorDataGridView.Location = new System.Drawing.Point(35, 92);
+            this.doctorDataGridView.Margin = new System.Windows.Forms.Padding(4);
+            this.doctorDataGridView.Name = "doctorDataGridView";
+            this.doctorDataGridView.RowHeadersWidth = 51;
+            this.doctorDataGridView.Size = new System.Drawing.Size(292, 199);
+            this.doctorDataGridView.TabIndex = 1;
+            this.doctorDataGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.doctorDataGridView_CellContentClick);
+            // 
+            // Combobox_miasta
+            // 
+            this.Combobox_miasta.FormattingEnabled = true;
+            this.Combobox_miasta.Location = new System.Drawing.Point(52, 357);
+            this.Combobox_miasta.Margin = new System.Windows.Forms.Padding(4);
+            this.Combobox_miasta.Name = "Combobox_miasta";
+            this.Combobox_miasta.Size = new System.Drawing.Size(272, 24);
+            this.Combobox_miasta.TabIndex = 2;
+            this.Combobox_miasta.SelectedIndexChanged += new System.EventHandler(this.Combobox_miasta_SelectedIndexChanged);
+            // 
+            // Umowionespotkaniabtn
+            // 
+            this.Umowionespotkaniabtn.Location = new System.Drawing.Point(69, 443);
+            this.Umowionespotkaniabtn.Margin = new System.Windows.Forms.Padding(4);
+            this.Umowionespotkaniabtn.Name = "Umowionespotkaniabtn";
+            this.Umowionespotkaniabtn.Size = new System.Drawing.Size(161, 26);
+            this.Umowionespotkaniabtn.TabIndex = 4;
+            this.Umowionespotkaniabtn.Text = "umowione spotkania";
+            this.Umowionespotkaniabtn.UseVisualStyleBackColor = true;
+            this.Umowionespotkaniabtn.Click += new System.EventHandler(this.Umowionespotkaniabtn_Click);
+            // 
+            // umow_wizyte_btn
+            // 
+            this.umow_wizyte_btn.Location = new System.Drawing.Point(69, 476);
+            this.umow_wizyte_btn.Margin = new System.Windows.Forms.Padding(4);
+            this.umow_wizyte_btn.Name = "umow_wizyte_btn";
+            this.umow_wizyte_btn.Size = new System.Drawing.Size(161, 26);
+            this.umow_wizyte_btn.TabIndex = 5;
+            this.umow_wizyte_btn.Text = "umow wizyte";
+            this.umow_wizyte_btn.UseVisualStyleBackColor = true;
+            this.umow_wizyte_btn.Click += new System.EventHandler(this.umow_wizyte_btn_Click);
+            // 
+            // Szukaj_btn
+            // 
+            this.Szukaj_btn.Location = new System.Drawing.Point(349, 357);
+            this.Szukaj_btn.Name = "Szukaj_btn";
+            this.Szukaj_btn.Size = new System.Drawing.Size(75, 23);
+            this.Szukaj_btn.TabIndex = 6;
+            this.Szukaj_btn.Text = "szukaj";
+            this.Szukaj_btn.UseVisualStyleBackColor = true;
+            this.Szukaj_btn.Click += new System.EventHandler(this.Szukaj_btn_Click);
+            // 
+            // umowione_wizyty1
+            // 
+            this.umowione_wizyty1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("umowione_wizyty1.BackgroundImage")));
+            this.umowione_wizyty1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.umowione_wizyty1.Location = new System.Drawing.Point(793, 382);
+            this.umowione_wizyty1.Margin = new System.Windows.Forms.Padding(5);
+            this.umowione_wizyty1.Name = "umowione_wizyty1";
+            this.umowione_wizyty1.Size = new System.Drawing.Size(238, 162);
+            this.umowione_wizyty1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1032, 546);
+            this.Controls.Add(this.Szukaj_btn);
+            this.Controls.Add(this.umow_wizyte_btn);
+            this.Controls.Add(this.Umowionespotkaniabtn);
+            this.Controls.Add(this.Combobox_miasta);
+            this.Controls.Add(this.doctorDataGridView);
+            this.Controls.Add(this.umowione_wizyty1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.doctorDataGridView)).EndInit();
+            this.ResumeLayout(false);
 
         }
 
@@ -117,9 +126,9 @@
 		private Umowione_wizyty umowione_wizyty1;
 		private System.Windows.Forms.DataGridView doctorDataGridView;
 		private System.Windows.Forms.ComboBox Combobox_miasta;
-		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.Button Umowionespotkaniabtn;
 		private System.Windows.Forms.Button umow_wizyte_btn;
-	}
+        private System.Windows.Forms.Button Szukaj_btn;
+    }
 }
 
--- ProjektProgramowanie_MainPage/Form1.cs
+++ ProjektProgramowanie_MainPage/Form1.cs
@@ -15,7 +15,8 @@ namespace ProjektProgramowanie_MainPage
     public partial class Form1 : Form
     {
         public static int user_id;
-		private SqlConnection con = new SqlConnection(@"Server=tcp:onlinegradebook.database.windows.net,1433;Initial Catalog=StoMaToLogiczne;Persist Security Info=False;User ID=theedziu;Password=Kacper123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+		public static string city;
+		private SqlConnection cn = new SqlConnection(@"Server=tcp:onlinegradebook.database.windows.net,1433;Initial Catalog=StoMaToLogiczne;Persist Security Info=False;User ID=theedziu;Password=Kacper123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
 		public Form1()
         {
             InitializeComponent();
@@ -26,6 +27,20 @@ namespace ProjektProgramowanie_MainPage
 		private void Form1_Load(object sender, EventArgs e)
 		{
 			umowione_wizyty1.Hide();
+			SqlCommand cmd = new SqlCommand();
+			cmd.Connection = cn;
+			cmd.CommandText = "SELECT DISTINCT city FROM doctor ";
+			cn.Open();
+			SqlDataReader reader;
+			reader = cmd.ExecuteReader();
+
+			while (reader.Read())
+			{
+				string city = reader["city"].ToString();
+				Combobox_miasta.Items.Add(city);
+			}
+			reader.Close();
+			cn.Close();
 		}
         public void Id(int id)
         {
@@ -45,6 +60,44 @@ namespace ProjektProgramowanie_MainPage
 			umowione_wizyty1.Show();
 			umowione_wizyty1.BringToFront();
 		}
-	}
+
+        private void Szukaj_btn_Click(object sender, EventArgs e)
+        {
+            cn.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = cn;
+            cmd.CommandText = "SELECT first_name, last_name, id, specialization FROM doctor WHERE city=@pam1";
+			cmd.Parameters.AddWithValue("@pam1", city);
+			DataTable data = new DataTable();
+			SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+			adapter.Fill(data);
+			doctorDataGridView.DataSource = data;
+			DataGridViewButtonColumn wybierz = new DataGridViewButtonColumn();
+			wybierz.Name = "Umów";
+            wybierz.Text = "Umów";
+            wybierz.UseColumnTextForButtonValue = true;
+			int len = doctorDataGridView.Columns.Count; // sprawdza ile kolumn zanim doda przycisk
+			doctorDataGridView.Columns.Insert(len, wybierz);
+            cn.Close();
+        }
+
+        private void Combobox_miasta_SelectedIndexChanged(object sender, EventArgs e)
+        {
+			city = Combobox_miasta.Text;// przypisanie z cmbbox do zmiennenj city
+        }
+
+        private void doctorDataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+			if (e.RowIndex >= 0)
+			{
+				DataGridViewRow row = this.doctorDataGridView.Rows[e.RowIndex];
+				int id = int.Parse(row.Cells["id"].Value.ToString());
+                 //pobieramy wartosci z datagrv
+				 //id visible na false z umowione wizyty datagrind
+
+            }
+
+        }
+    }
 
 }
